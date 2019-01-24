98854090b618d4bbb79fd9262f1a9c549920317d
dingjuan <1587830150@qq.com>
2019-01-24 15:33:24 +0700

    Signed-off-by: dingjuan <1587830150@qq.com>

--- src/router/router.js
+++ src/router/router.js
@@ -1,9 +1,9 @@
 import App from '../App'
 
 const home = r => require.ensure([], () => r(require('../page/home/home')), 'home')
-
-
-
+const city = r => require.ensure([], () => r(require('../page/city/city')), 'city')
+const login = r => require.ensure([], () => r(require('../page/login/login')), 'login')
+const profile = r => require.ensure([], () => r(require('../page/profile/profile')), 'profile')
 
 export default [{
     path: '/',
@@ -19,6 +19,38 @@ export default [{
             path: '/home',
             component: home
         },
-
+        //当前选择城市页
+        {
+          path: '/city/:cityid',
+          component: city
+        },
+        //登录注册页
+        {
+          path: '/login',
+          component: login
+        },
+      //个人信息页
+      {
+        path: '/profile',
+        component: profile,
+        // children: [{
+        //   path: 'info', //个人信息详情页
+        //   component: info,
+        //   children: [{
+        //     path: 'setusername',
+        //     component: setusername,
+        //   },{
+        //     path: 'address',
+        //     component: address,     //编辑地址
+        //     children:[{
+        //       path:'add',
+        //       component:add,
+        //       children:[{
+        //         path:'addDetail',
+        //         component:addDetail
+        //       }]
+        //     }]
+        //   }]
+        },
     ]
 }]
