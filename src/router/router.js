22f9967e439dbb483cf351a569fc0e7740a52c13
dingjuan <1587830150@qq.com>
2019-01-30 18:53:22 +0700

    特色商铺列表页 编辑地址

--- src/router/router.js
+++ src/router/router.js
@@ -6,12 +6,15 @@ const msite = r => require.ensure([], () => r(require('../page/msite/msite')), '
 const login = r => require.ensure([], () => r(require('../page/login/login')), 'login')
 const profile = r => require.ensure([], () => r(require('../page/profile/profile')), 'profile')
 const info = r => require.ensure([], () => r(require('../page/profile/children/info')), 'info')
+const setusername = r => require.ensure([], () => r(require('../page/profile/children/children/setusername')), 'setusername')
+const address = r => require.ensure([], () => r(require('../page/profile/children/children/address')), 'address')
 const shop = r => require.ensure([], () => r(require('../page/shop/shop')), 'shop')
 const foodDetail = r => require.ensure([], () => r(require('../page/shop/children/foodDetail')), 'foodDetail')
 const shopDetail = r => require.ensure([], () => r(require('../page/shop/children/shopDetail')), 'shopDetail')
 const shopSafe = r => require.ensure([], () => r(require('../page/shop/children/children/shopSafe')), 'shopSafe')
 const search = r => require.ensure([], () => r(require('../page/search/search')), 'search')
-
+const food = r => require.ensure([], () => r(require('../page/food/food')), 'food')
+const order = r => require.ensure([], () => r(require('../page/order/order')), 'order')
 export default [{
   path: '/',
   component: App, //顶层路由，对应index.html
@@ -30,6 +33,11 @@ export default [{
     {
       path: '/city/:cityid',
       component: city
+    },
+    //特色商铺列表页
+    {
+      path: '/food',
+      component: food
     },
     //所有商铺列表页
     {
@@ -65,38 +73,30 @@ export default [{
       path: '/login',
       component: login
     },
-    //个人信息页
     {
       path: '/profile',
       component: profile,
       children: [{
         path: 'info', //个人信息详情页
         component: info,
-        // children: [{
-        //   path: 'setusername',
-        //   component: setusername,
-        // }]
+        children: [{
+          path: 'setusername',
+          component: setusername,
+        },{
+          path: 'address',
+          component: address,     //编辑地址
+        }]
       }]
-
-      //     {
-      //     path: 'address',
-      //     component: address,     //编辑地址
-      //     children:[{
-      //       path:'add',
-      //       component:add,
-      //       // children:[{
-      //       //   path:'addDetail',
-      //       //   component:addDetail
-      //       // }]
-      //     }]
-      //   }]
-      // },
-      //   {
-      //     path: 'service', //服务中心
-      //     component: service,
-      //   },]
     },
-
+    //订单列表页
+    {
+      path: '/order',
+      component: order,
+      // children: [{
+      //   path: 'orderDetail', //订单详情页
+      //   component: orderDetail,
+      // }, ]
+    },
 
   ]
 }]
